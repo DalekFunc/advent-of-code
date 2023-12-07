9bd56c9ab694fbdf72d3821e2108eb33115b505f
Hin Kwan Chan <wavefunctioncollapse@icloud.com>
2023-12-07 11:53:30 +0800

    + Import 2023 day 2.

---
+++ year-2023/rust/day-2/src/bin/part1.rs
@@ -0,0 +1,25 @@
+use day_2::part1;
+
+fn main() {
+    let result = part1(include_str!("../../input.txt")).expect("Part 1 failed to run");
+
+    println!("{result}");
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    use super::*;
+
+    #[test]
+    fn quick_test() {}
+
+    #[rstest]
+    #[case(8)]
+    fn test_using_test_input_1(#[case] expected: u32) {
+        let result = part1(include_str!("../../test-1.txt")).expect("Part 1 failed to run");
+
+        assert_eq!(expected, result);
+    }
+}
