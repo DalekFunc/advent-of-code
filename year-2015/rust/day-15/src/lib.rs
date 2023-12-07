4894e9b29e71e441a812d239291fe6f83c8a657e
Hin Kwan Chan <wavefunctioncollapse@icloud.com>
2023-12-07 21:42:35 +0800

    + Solution for 2015 day 15 part 1 and 2.
    Change toolchain to nightly to use unstable rust feature like div_ceil

--- year-2015/rust/day-15/src/lib.rs
+++ year-2015/rust/day-15/src/lib.rs
@@ -20,13 +20,143 @@ pub fn part1(input: &str) -> Result<u64> {
         .map(|line| parse_line(line).unwrap().1)
         .collect();
 
-    // narrow down acceptable ranges for each ingred.
+    println!("{ingreds:?}");
 
-    Ok(0)
+    // narrow down acceptable ranges for each ingred.
+    let mut ranges = [0..101, 0..101, 0..101, 0..101];
+    for (left, right) in [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)] {
+        for idx in 0..4 {
+            let (ll, rr) = get_range_limits(ingreds[left][idx], ingreds[right][idx], 100);
+            println!("> {:?}, {:?}", ll, rr);
+            ranges[left] = max(ranges[left].start, ll.start)..min(ranges[left].end, ll.end);
+            ranges[right] = max(ranges[right].start, rr.start)..min(ranges[right].end, rr.end);
+        }
+    }
+
+    println!("{:?}", ranges);
+
+    // brute force
+    let mut score = 0;
+    for i in ranges[0].clone() {
+        for j in ranges[1].start..ranges[1].end {
+            if i + j >= 100 {
+                break;
+            }
+            for k in ranges[2].start..ranges[2].end {
+                if i + j + k >= 100 {
+                    break;
+                }
+                let l = 100 - i - j - k;
+
+                // println!("{} {} {} {}", i, j, k, l);
+                let mut ingreds = ingreds.clone();
+                [i, j, k, l].iter().enumerate().for_each(|(idx, quantity)| {
+                    ingreds[idx].iter_mut().for_each(|value| {
+                        *value *= quantity;
+                    })
+                });
+                // dbg!(&ingreds);
+
+                let sum: Vec<i64> = (0..4)
+                    .map(|idx| {
+                        let prop: i64 = ingreds.iter().map(|ingred| ingred[idx]).sum();
+                        if prop.is_negative() {
+                            0
+                        } else {
+                            prop
+                        }
+                    })
+                    .collect();
+                let new_score = sum.iter().product();
+
+                score = max(score, new_score);
+            }
+        }
+    }
+
+    Ok(score as u64)
 }
 
 pub fn part2(input: &str) -> Result<u64> {
-    Err(anyhow!("Not Implemented."))
+    let ingreds: Vec<Vec<i64>> = input
+        .lines()
+        .map(|line| parse_line(line).unwrap().1)
+        .collect();
+
+    println!("{ingreds:?}");
+
+    // narrow down acceptable ranges for each ingred.
+    let mut ranges = [0..101, 0..101, 0..101, 0..101];
+    for (left, right) in [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)] {
+        for idx in 0..4 {
+            let (ll, rr) = get_range_limits(ingreds[left][idx], ingreds[right][idx], 100);
+            println!("> {:?}, {:?}", ll, rr);
+            ranges[left] = max(ranges[left].start, ll.start)..min(ranges[left].end, ll.end);
+            ranges[right] = max(ranges[right].start, rr.start)..min(ranges[right].end, rr.end);
+        }
+    }
+
+    println!("{:?}", ranges);
+
+    // brute force
+    let mut score = 0;
+    // for i in ranges[0].clone() {
+    //     for j in ranges[1].start..ranges[1].end {
+    //         if i + j >= 100 {
+    //             break;
+    //         }
+    //         for k in ranges[2].start..ranges[2].end {
+    for i in 0..101 {
+        for j in 0..101 {
+            if i + j >= 100 {
+                break;
+            }
+            for k in 0..101 {
+                if i + j + k >= 100 {
+                    break;
+                }
+                let l = 100 - i - j - k;
+
+                let total_calorie: i64 = ingreds
+                    .iter()
+                    .zip([i, j, k, l])
+                    .map(|(ingred, quantity)| ingred[4] * quantity)
+                    .sum();
+
+                if total_calorie != 500 {
+                    continue;
+                }
+
+                let mut ingreds = ingreds.clone();
+                [i, j, k, l].iter().enumerate().for_each(|(idx, quantity)| {
+                    ingreds[idx].iter_mut().for_each(|value| {
+                        *value *= quantity;
+                    })
+                });
+                // dbg!(&ingreds);
+
+                let sum: Vec<i64> = (0..4)
+                    .map(|idx| {
+                        let prop: i64 = ingreds.iter().map(|ingred| ingred[idx]).sum();
+                        if prop.is_negative() {
+                            0
+                        } else {
+                            prop
+                        }
+                    })
+                    .collect();
+                let new_score = sum.iter().product();
+
+                if new_score > 0 {
+                    println!("{} {} {} {} {}", i, j, k, l, new_score);
+                }
+
+                score = max(score, new_score);
+            }
+        }
+    }
+
+    Ok(score as u64)
 }
 
 // region:    --- Parser
@@ -48,7 +178,9 @@ fn parse_line(input: &str) -> IResult<&str, Vec<i64>> {
 // region:    --- Part 1
 // L, R: -1, 2 -> 0..34, 67..101
 fn get_range_limits(lhs: i64, rhs: i64, total: i64) -> (Range<i64>, Range<i64>) {
-    if lhs > 0 && rhs > 0 {
+    if !lhs.is_positive() && !rhs.is_positive() {
+        (0..total + 1, 0..total + 1) // if both are negative or zero, there are nothing we can do to make their sum positive
+    } else if lhs > 0 && rhs > 0 {
         (0..total + 1, 0..total + 1)
     } else if lhs > 0 && rhs == 0 || lhs == 0 && rhs > 0 {
         if lhs.is_positive() {
@@ -97,7 +229,18 @@ mod tests {
         let l = ls
             .into_iter()
             .reduce(|lhs, rhs| max(lhs.start, rhs.start)..min(lhs.end, rhs.end));
-        println!("{:?}", l)
+        println!("{:?}", l);
+
+        let mut l = 0..101;
+        let mut r = 0..101;
+        for idx in 0..4 {
+            let (ll, rr) = get_range_limits(ingreds[0][idx], ingreds[1][idx], 100);
+            println!("> {:?}, {:?}", ll, rr);
+            l = max(l.start, ll.start)..min(l.end, ll.end);
+            r = max(r.start, rr.start)..min(r.end, rr.end);
+        }
+
+        println!("{l:?}, {r:?}");
     }
 
     #[rstest]
