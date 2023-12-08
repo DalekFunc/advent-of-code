175f16cccc3ece7a130a305a5829ae07c1efbf93
Hin Kwan Chan <wavefunctioncollapse@icloud.com>
2023-12-08 14:48:03 +0800

    . begin 2023 day 8.

---
+++ year-2023/rust/day-8/src/lib.rs
@@ -0,0 +1,19 @@
+use anyhow::{Result, anyhow};
+
+pub fn part1(input: &str) -> Result<u64> {
+    Err(anyhow!("Not Implemented."))
+}
+
+pub fn part2(input: &str) -> Result<u64> {
+    Err(anyhow!("Not Implemented."))
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    use super::*;
+
+    #[test]
+    fn quick_test() {}
+}
