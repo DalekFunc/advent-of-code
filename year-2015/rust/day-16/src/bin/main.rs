12a97f4820c328323d656d81cf2c97a228c48ff9
Hin Kwan Chan <wavefunctioncollapse@icloud.com>
2023-12-09 18:17:24 +0800

    . begin 2015 day 16.

---
+++ year-2015/rust/day-16/src/bin/main.rs
@@ -0,0 +1,12 @@
+use day_16::part1;
+use day_16::part2;
+
+fn main() {
+    let result = part1(include_str!("../../input.txt"));
+
+    println!("Part 1: {result:?}");
+
+    let result = part2(include_str!("../../input.txt"));
+
+    println!("Part 2: {result:?}");
+}
