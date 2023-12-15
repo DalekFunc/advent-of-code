5fcfd78f8fc5fc40509f367526487bc0f021ebcb
DalekFunc <wavefunctioncollapse@icloud.com>
2023-12-15 21:44:34 +0800

    . 2023 day 14 part 2 in progress.

--- year-2023/rust/day-14/src/bin/main.rs
+++ year-2023/rust/day-14/src/bin/main.rs
@@ -3,11 +3,11 @@ use day_14::part2;
 
 fn main() {
     let result = part1(include_bytes!("../../input.txt"));
-    // let result = part1(include_bytes!("../../test-1.txt"));
 
     println!("Part 1: {result:?}");
 
-    let result = part2(include_str!("../../input.txt"));
+    let result = part2(include_bytes!("../../input.txt"));
+    let result = part2(include_bytes!("../../test-1.txt"));
 
     println!("Part 2: {result:?}");
 }
--- year-2023/rust/day-14/src/lib.rs
+++ year-2023/rust/day-14/src/lib.rs
@@ -47,8 +47,48 @@ pub fn part1(input: &[u8]) -> Result<u64> {
     Ok(score)
 }
 
-pub fn part2(input: &str) -> Result<u64> {
-    Err(anyhow!("Not Implemented."))
+pub fn part2(input: &[u8]) -> Result<u64> {
+    let (_, input) = parse_map(input).expect("parse ok");
+
+    let mut grid = Grid(vec![vec![Rock::Empty; input.len() + 2]; input[0].len() + 2]);
+    let grid_width = input.len() + 2;
+    let grid_height = input[0].len() + 2;
+
+    // fill first and last column with Cube
+    for row in 0..grid_height {
+        grid.0[row][grid_width-1] = Rock::Cube;
+    }
+    for row in 0..grid_height {
+        grid.0[row][0] = Rock::Cube;
+    }
+    // fill first and last row with Cube
+    grid.0[0].fill(Rock::Cube);
+    grid.0[grid_height - 1].fill(Rock::Cube);
+
+    for (to_col, from_row) in (0..input.len()).rev().enumerate() {
+        for idx in 0..input[0].len() {
+            grid.0[idx+1][to_col+1] = input[from_row][idx].into();
+        }
+    }
+    println!("{}", grid);
+
+    for cycle_count in 0..100 {
+        dbg!(cycle_count);
+        grid.cycle();
+        println!("{}", grid);
+    }
+
+
+    let mut score = 0;
+    for row in 0..grid.0.len() {
+        score += grid.0[row].iter().fold((0, 1), |(acc, point), &elem|{ if elem == Rock::Round {
+            (acc + point, point + 1)
+        } else {
+            (acc, point + 1)
+        }}).0;
+    }
+
+    Ok(score)
 }
 
 // region:    --- Parsing
@@ -90,6 +130,45 @@ impl std::fmt::Display for Rock {
 }
 
 struct Grid(Vec<Vec<Rock>>);
+
+impl Grid {
+    fn rotate(&mut self) {
+        let mut matrix = vec![vec![Rock::Empty; self.0.len()]; self.0[0].len()];
+
+        for (to_col, from_row) in (0..self.0.len()).rev().enumerate() {
+            for idx in 0..self.0[0].len() {
+                matrix[idx][to_col] = self.0[from_row][idx].into();
+            }
+        }
+
+        self.0 = matrix;
+    }
+
+    fn rolling(&mut self) {
+        for row in 0..self.0.len() {
+            let mut positions = self.0[row].iter().positions(|&rock| rock == Rock::Cube).collect_vec();
+
+
+            self.0[row][0..positions[0]].sort();
+            for (&from, &to) in positions.iter().tuple_windows() {
+                if from + 1 < self.0[0].len() {
+                    self.0[row][from + 1..to].sort();
+                }
+            }
+        }
+    }
+
+    fn cycle(&mut self) {
+        self.rolling();
+        self.rotate(); // west
+        self.rolling();
+        self.rotate(); // south
+        self.rolling();
+        self.rotate(); //east
+        self.rolling();
+        self.rotate(); // back to north
+    }
+}
 
 impl std::fmt::Display for Grid {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
