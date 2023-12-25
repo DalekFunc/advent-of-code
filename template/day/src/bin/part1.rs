f4bb6ba137bc0d65898ca732f57032c3644c2912
DalekFunc <wavefunctioncollapse@icloud.com>
2023-12-25 16:46:44 +0800

    . modify testing template.

--- template/day/src/bin/part1.rs
+++ template/day/src/bin/part1.rs
@@ -5,21 +5,3 @@ fn main() {
 
     println!("{result}");
 }
-
-#[cfg(test)]
-mod tests {
-    use rstest::rstest;
-
-    use super::*;
-
-    #[test]
-    fn quick_test() {}
-
-    #[rstest]
-    #[case(0)]
-    fn test_using_test_input_1(#[case] expected: u64) {
-        let result = part1(include_str!("../../test-1.txt")).expect("Part 1 failed to run");
-
-        assert_eq!(expected, result);
-    }
-}
--- template/day/src/lib.rs
+++ template/day/src/lib.rs
@@ -16,4 +16,20 @@ mod tests {
 
     #[test]
     fn quick_test() {}
+
+    #[rstest]
+    #[case(0)]
+    fn part_1_using_test_input_1(#[case] expected: u64) {
+        let result = part1(include_str!("../test-1.txt")).expect("Part 1 failed to run");
+
+        assert_eq!(expected, result);
+    }
+
+    #[rstest]
+    #[case(0)]
+    fn part_2_using_test_input_1(#[case] expected: u64) {
+        let result = part2(include_str!("../test-1.txt")).expect("Part 2 failed to run");
+
+        assert_eq!(expected, result);
+    }
 }
